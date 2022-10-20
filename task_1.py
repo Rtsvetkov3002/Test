892d1e84c7c94f9aca9e5e54625dc6b43d29662f
Roman Tsvetkov <76113745+Rtsvetkov3002@users.noreply.github.com>
2022-10-20 22:38:16 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,9 +1,9 @@
+#!/usr/bin/env python3
 """
 not missing docstring
 """
 
-#!/usr/bin/env python3  # указывает Unix-подобным системам, какой интерпретатор использовать
-# -*- coding: utf-8 -*-  # исполизуется кодировка UTF-8
+# -*- coding: utf-8 -*-
 
 import math  # подключение библиотеки math
 import numpy  # подключение библиотеки numpy
@@ -13,7 +13,7 @@ import matplotlib.pyplot as mpp  # подключение библиотеки M
 
 if __name__=='__main__':  # если файл запущен напрямую (не как подключенный модуль):
     arguments = numpy.arange(0, 200, 0.1)  # создает массив из чисел [0, 200) с шагом 0.1
-    mpp.plot(  # строит график: 
+    mpp.plot(  # строит график:
         arguments,  # берет числа из созданного ранее массива
         [math.sin(a) * math.sin(a/20.0) for a in arguments]  # и считает значения <- этой функции
     )
