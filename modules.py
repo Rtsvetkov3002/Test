e5ceefb73099e122a843d2fa6ad72b2759771e87
Roman Tsvetkov <76113745+Rtsvetkov3002@users.noreply.github.com>
2023-04-23 18:30:49 +0300

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,19 @@
+"""
+Modules testing
+"""
+import numpy
+from numpy import array
+from gauss import gauss
+
+a = array([
+    [1.5, 2.0, 1.5, 2.0],
+    [3.0, 2.0, 4.0, 1.0],
+    [1.0, 6.0, 0.0, 4  ],
+    [2.0, 1.0, 4.0, 3  ]
+], dtype=float)
+
+b = array([5, 6, 7, 8], dtype=float)
+
+solution = gauss(a, b)
+
+print(solution)
