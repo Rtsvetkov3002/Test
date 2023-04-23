58d6ee67e92ed2b76338e02edc1d1c3b3adb28e1
Roman Tsvetkov <76113745+Rtsvetkov3002@users.noreply.github.com>
2023-04-23 22:10:35 +0300

    Update monte.py

--- monte.py
+++ monte.py
@@ -12,17 +12,16 @@ def monte_euler():
     Int(e**(-x**2)) = sqrt(pi), функция четная
     """
     count = 0
-    X = 2.7 # Чем больше Х, тем больше нужно брать it
+    X_max = 2.7 # Чем больше Х, тем больше нужно брать it
     for i in range(ITER):
-        x = random.uniform(0, X)
+        x = random.uniform(0, X_max)
         y = random.uniform(0, 1)
         eul = math.e**(-x**2)
         if (y <= eul):
             count += 1
         i += 1
-    approx = 1*X * count / ITER
+    approx = 1 * X_max * count / ITER
     return approx
 
-Area = monte_euler()
-print((2*Area)**2 - math.pi)
+print((2*monte_euler())**2 - math.pi)
 #print(timeit.timeit(monte_euler, number=1))  # ~55,98 ITER = 100_000_000
